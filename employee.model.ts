64c4e9d456d6a8a3829acf2588b4278144000cae
joydip-git <34733254+joydip-git@users.noreply.github.com>
2020-02-13 14:22:37 +0530

    uploaded employee code

---
+++ employee.model.ts
@@ -0,0 +1,23 @@
+export class Employee {
+    public get salary(): number {
+        return this._salary;
+    }
+    public set salary(value: number) {
+        this._salary = value;
+    }
+    public get name(): string {
+        return this._name;
+    }
+    public set name(value: string) {
+        this._name = value;
+    }
+    public get id(): number {
+        return this._id;
+    }
+    public set id(value: number) {
+        this._id = value;
+    }
+    constructor(private _id: number, private _name: string, private _salary: number) {
+
+    }
+}
